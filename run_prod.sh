ceba5ca13c7abbc530baf71724759ede358ad9ce
FranekStratovarius <louishuss2000@gmail.com>
2022-12-19 19:48:22 +0100

    port is customizable

--- run_prod.sh
+++ run_prod.sh
@@ -1,1 +1,8 @@
-PORT=80 MIX_ENV=prod mix phx.server
+if [[ -z "${PORT}" ]]; then
+	# fine, just use default 80 port
+	PORT="80"
+else
+	
+	# use PORT and dont change anything
+fi
+MIX_ENV=prod mix phx.server
